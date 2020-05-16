40fe763e96365921ab52b7e8fae6651c27411569
Keita <keita04141537@icloud.com>
2020-05-16 17:08:47 +0000

    admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   before_action :set_one_month, only: :show
 
   def index
-    @users = User.paginate(page: params[:page]).search(params[:search])
+    @users = User.paginate(page: params[:page])
+    
   end
 
   def show
