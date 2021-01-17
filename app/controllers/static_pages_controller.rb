3002facde346128557dc02276abb8318672aa820
masa0520 <sample.email.com>
2021-01-17 11:43:56 +0900

    add all_index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @first_day = params[:date].nil? ?
+    Date.current : params[:date].to_date
+    @last_day = @first_day.since(7.days)
+    @book_records = BookRecord.where(record_date: @first_day..@last_day).order(:record_date)
   end
 
   def help
